aa828040e4b6f85353195e724127603ba2fdf7b4
irfan340 <61534088+irfan340@users.noreply.github.com.>
2025-02-21 17:00:15 +0600

    test

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,7 +1,8 @@
 const Navbar = () => {
     return (
         <div>
-            <h1>Navbar</h1>            
+            <h1>Navbar</h1>  
+            <h2>here</h2>          
         </div>
     );
 };
